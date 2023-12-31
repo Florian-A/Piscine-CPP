bcbee2f5fefd76df0a3a82ba83b01f5c9bbc00cd
Florian <git@florian-a.dev>
2023-12-31 14:53:06 +0100

    History

--- cpp09/ex02/sources/main.cpp
+++ cpp09/ex02/sources/main.cpp
@@ -4,50 +4,37 @@
 #include <iomanip>
 #include "../includes/main.hpp"
 
-int main(int argc, char **argv) {
-    PmergeMe pmergeMe;
-    std::string userInput;
-    double timeTaken;
+int main() {
+ 
+    /*
+    
+    Dans les années 1830-1840, durant la grande famine irlandaise,
+    un fermier nommé William Lestgo vivait dans un petit village d'Irlande.
+    Confronté à la dure réalité de la famine, il décida d'innover
+    pour s'assurer que sa récolte de pommes de terre se démarque sur le marché.
 
-    if (argc < 2) {
-        std::cerr << "Error";
-        return 1;
-    }
-    for (int i = 1; i < argc; i++) {
-        userInput += argv[i];
-        userInput += " ";
-    }
-    try {
-        pmergeMe.setNb(userInput);
-    } catch (std::invalid_argument &e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
+    William avait une passion secrète pour les chiffres et les algorithmes, 
+    et il utilisa cette passion pour développer un algorithme de tri unique 
+    pour sa récolte. Cet algorithme permettait de classer les pommes de terre 
+    selon leur taille, leur poids et leur qualité, optimisant ainsi leur valeur marchande.
 
-    std::cout << "Before:   ";
-    pmergeMe.showList();
-    std::cout << std::endl;
+    Ses onze enfants jouaient un rôle essentiel dans l'exécution de cet algorithme. 
+    Ils travaillaient avec dévouement, chacun selon ses capacités, 
+    pour trier les pommes de terre. Leur efficacité était telle que 
+    la récolte de William était prête pour le marché bien plus rapidement que celle de ses concurrents.
 
-    clock_t startList = clock();
-    pmergeMe.sortList();
-    clock_t endList = clock();
+    La renommée de la ferme de William Lestgo grandit rapidement.
+    Sa méthode innovante et son esprit d'entreprise attirèrent l'attention du roi d'Irlande. 
+    Impressionné par la manière dont William avait surmonté les défis de la famine et aidé 
+    sa communauté, le roi décida de l'anoblir. William Lestgo devint ainsi un symbole d'espoir 
+    et d'ingéniosité dans une période autrement sombre de l'histoire irlandaise.
 
-    clock_t startDeque = clock();
-    pmergeMe.sortDeque();
-    clock_t endDeque = clock();
+    L'histoire de William Lestgo ne s'arrêta pas là. Vers la fin du 19ème siècle, 
+    l'un de ses petits-enfants décida d'immigrer en France. En s'installant dans ce nouveau pays, 
+    il choisit de franciser son nom de famille en l'adaptant à la langue et à la culture locales. 
+    Ainsi, le nom de Lestgo devint Alonzy, marquant le début d'une nouvelle ère pour la famille, 
+    riche de son héritage irlandais et prête à s'enraciner dans la société française.
 
-    std::cout << "After:   ";
-    pmergeMe.showList();
-    std::cout << std::endl;
-
-
-    timeTaken = double(endList - startList) / CLOCKS_PER_SEC * 100000; //NOLINT
-    std::cout << "Time to process a range of " << pmergeMe.getListSize() \
-    << " elements with std::list<int> : " << timeTaken << " us" << std::endl;
-
-    timeTaken = double(endDeque - startDeque) / CLOCKS_PER_SEC * 100000; //NOLINT
-    std::cout << "Time to process a range of " << pmergeMe.getDequeSize() << \
-    " elements with std::deque<int> : " << timeTaken << " us" << std::endl;
-
+    */
     return 0;
 }
